050b857d2adc27f88bfff019cfcba5ee618a48b3
12063 <swatibhojwani.24@gmail.com>
2021-04-21 14:55:44 -0700

    Final project

--- src/Detail/Detail.jsx
+++ src/Detail/Detail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./DetailItem/DetailItem.module.css";
+import styles from "../DetailItem/DetailItem.module.css";
 import PropTypes from "prop-types";
 import DetailItem from "../DetailItem/DetailItem.jsx";
 
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     return (
       <div className={styles.homeContainer}>
       <h1 className={styles.homeTitle}>Welcome to My Home Recipes!!!</h1>
-           <img className={styles.homeImg} src="https://i.keaitupian.net/up/7b/e9/bb/c48adec6c91a1ed2ec5e6b7f65bbe97b.jpg" alt="Homepage"/>
-      </div>
+       <img className={styles.homeImg} src="https://i.keaitupian.net/up/7b/e9/bb/c48adec6c91a1ed2ec5e6b7f65bbe97b.jpg" alt="Homepage"/>
+                      </div>
     )
 }
 
