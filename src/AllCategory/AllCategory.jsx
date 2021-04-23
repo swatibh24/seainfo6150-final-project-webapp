63bca49a92cc6fa8cf5a708211a96e2072ac7414
12063 <swatibhojwani.24@gmail.com>
2021-04-22 17:46:05 -0700

    Final project

--- src/AllCategory/AllCategory.jsx
+++ src/AllCategory/AllCategory.jsx
@@ -4,29 +4,29 @@ import styles from "./AllCategory.module.css";
 
 const AllCategory = () => {
   return (
-    <div className={styles.allCategoryContainer}>
-      <h2 className={styles.allCategoryDetail}>Pick the categories you would like to explore!
+    <div className={styles.allCatContainer}>
+      <h2 className={styles.allCatDetail}>Pick the categories you would like to explore!
       </h2>
     <div className={styles.ListContainer}>
      <div className={styles.container}>
       <Link className={styles.styleCategories} to="/category/Soup">
-        <p className={styles.allCategoryName}>Soup</p>
+        <p className={styles.allCategoryTypes}>Soup</p>
       </Link>
       <Link className={styles.styleCategories} to="/category/Appetizer">
-        <p className={styles.allCategoryName}>Appetizer</p>
+        <p className={styles.allCategoryTypes}>Appetizer</p>
       </Link>
       <Link className={styles.styleCategories} to="/category/Snack">
-        <p className={styles.allCategoryName}>Snack</p>
+        <p className={styles.allCategoryTypes}>Snack</p>
       </Link>
       <Link className={styles.styleCategories} to="/category/Main-Course">
-        <p className={styles.allCategoryName}>Main Course</p>
+        <p className={styles.allCategoryTypes}>Main Course</p>
       </Link>
       <Link className={styles.styleCategories} to="/category/Dessert">
-        <p className={styles.allCategoryName}>Dessert</p>
+        <p className={styles.allCategoryTypes}>Dessert</p>
       </Link>
       </div>
       </div>
-         <img className={styles.allCategoryImg}
+         <img className={styles.allCatImg}
          src="https://c0.wallpaperflare.com/preview/301/367/805/black-bean-close-up-cooked-cuisine.jpg" alt="categories"/>
     </div>
   );
--- src/CategoryListItem/CategoryListItem.jsx
+++ src/CategoryListItem/CategoryListItem.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 import styles from "./CategoryListItem.module.css";
 
 const CategoryListItem = (props) => {
-  
-    const recipe=props.recipe;
+  const recipe=props.recipe;
     return (
       <div>
         <Link className={styles.ListContainer} to={recipe.detailURL}>
